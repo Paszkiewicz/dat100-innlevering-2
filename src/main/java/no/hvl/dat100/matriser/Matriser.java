42d777e12076cca768c63434f5bc92679a23cac3
hovringmarkus-crypto <hovringmarkus@gmail.com>
2025-09-27 19:12:57 +0200

    Matrise b

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -17,9 +17,16 @@ public class Matriser {
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+        String resultat = "";
 
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                resultat = resultat + matrise[i][j] + " ";
+            }
+            resultat = resultat + "\n";
+        }
+
+        return resultat;
 	}
 
 	// c)
